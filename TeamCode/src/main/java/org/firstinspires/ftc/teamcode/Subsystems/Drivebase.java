2ac0213e9b38317fef86a09077ff0d7e12ccfff4
NguyenTuanMi <ntuanminh0309@gmail.com>
2022-07-28 00:24:14 +0700

    Retrieve data from the sensors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Drivebase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Drivebase.java
@@ -2,29 +2,51 @@ package org.firstinspires.ftc.teamcode.Subsystems;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
 public class Drivebase {
-    private DcMotor leftMaster;
-    private DcMotor rightMaster;
+    private DcMotorEx leftMaster;
+    private DcMotorEx rightMaster;
+    private DcMotorEx leftFollow;
+    private DcMotorEx rightFollow;
     private HardwareMap hardwareMap;
+
     public Drivebase (OpMode opMode) {
         this.hardwareMap = opMode.hardwareMap;;
     }
 
     public void init() {
-        leftMaster = hardwareMap.dcMotor.get("lm");
-        rightMaster = hardwareMap.dcMotor.get("rm");
+        leftMaster = hardwareMap.get(DcMotorEx.class, "lm");
+        rightMaster = hardwareMap.get(DcMotorEx.class, "rm");
+        leftFollow = hardwareMap.get(DcMotorEx.class, "lf");
+        rightFollow = hardwareMap.get(DcMotorEx.class, "rf");
+
 
         leftMaster.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftFollow.setDirection(DcMotorSimple.Direction.REVERSE);
 
         leftMaster.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightMaster.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFollow.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFollow.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     public void setSpeed(double speed1, double speed2) {
         rightMaster.setPower(speed1);
         leftMaster.setPower(speed2);
+        rightFollow.setPower(speed1);
+        leftFollow.setPower(speed2);
+    }
+
+    public double getLeftVelocity() {
+        return leftMaster.getVelocity(AngleUnit.RADIANS);
+    }
+
+    public double getRightVelocity() {
+        return rightMaster.getVelocity(AngleUnit.RADIANS);
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/IMU.java
@@ -0,0 +1,94 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+public class IMU {
+    private BNO055IMU imu;
+    private Orientation angle;
+    private HardwareMap hardwareMap;
+    private AngularVelocity angularVelocity;
+    private Acceleration accel;
+
+    public IMU(OpMode opMode) {
+        this.hardwareMap = opMode.hardwareMap;
+    }
+
+    public void init() {
+        BNO055IMU.Parameters parameters = imu.getParameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        imu = hardwareMap.get(BNO055IMU.class, "IMU");
+        imu.initialize(parameters);
+    }
+
+    public void updateAngularVelocity() {
+        angularVelocity = imu.getAngularVelocity();
+    }
+
+    public void updateAngleOrientation() {
+        angle = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.RADIANS);
+    }
+
+    public void updateAccel() {
+        accel = imu.getAcceleration();
+    }
+
+    public AngularVelocity getAngularVelocity() {
+        return angularVelocity;
+    }
+
+    public Orientation getAngleOrientation() {
+        return angle;
+    }
+
+    public Acceleration getAccel() {
+        return accel;
+    }
+
+    public float getYaw() {
+        return getAngleOrientation().thirdAngle;
+    }
+
+    public float getRoll() {
+        return getAngleOrientation().firstAngle;
+    }
+
+    public float getPitch() {
+        return getAngleOrientation().secondAngle;
+    }
+
+    public float getRotaRateX() {
+        return getAngularVelocity().xRotationRate;
+    }
+
+    public float getRotaRateY() {
+        return getAngularVelocity().yRotationRate;
+    }
+
+    public float getRotaRateZ() {
+        return getAngularVelocity().zRotationRate;
+    }
+
+    public double getAccelX() {
+        return accel.xAccel;
+    }
+
+    public double getAccelY() {
+        return accel.yAccel;
+    }
+
+    public double getAccelZ() {
+        return accel.zAccel;
+    }
+
+
+}
